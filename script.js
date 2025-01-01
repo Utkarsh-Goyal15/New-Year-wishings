b6b24bc8f5b03b0bf8b4b489e714d9574e7e99c8
Utkarsh goyal <utkarshgoyal1503@gmail.com>
2025-01-01 23:31:56 +0530

    Update script.js with grammatical mistakes

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function newYear() {
         return;
     }
 
-    let wishes = `Happy new year ${x.value} and to Family <br> At midnight, a new year starts, With dreams and hopes in our hearts. We say goodbye to the old year, And welcome the new one with cheer.<br> New stories and adventures await, With each day we'll celebrate. Let's laugh, learn, and hold dear, The moments in this bright new year.<br>Happy New Year to you! 🎉`
+    let wishes = `Happy new year to ${x.value} and Family <br> At midnight, a new year starts, With dreams and hopes in our hearts. We say goodbye to the old year, And welcome the new one with cheer.<br> New stories and adventures await, With each day we'll celebrate. Let's laugh, learn, and hold dear, The moments in this bright new year.<br>Happy New Year to you! 🎉`
     wishDiv.innerHTML = wishes
     bg.appendChild(wishDiv);
     image.style.position = "relative";
